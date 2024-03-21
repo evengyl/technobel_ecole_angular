713fb04c0415e67f3ca5548b079f38f7dacbbadf
evengyl <baudouxloic@gmail.com>
2024-03-21 20:53:48 +0100

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-];
+  { path : "", component : HomePage }
+]
 
 @NgModule({
   imports: [
